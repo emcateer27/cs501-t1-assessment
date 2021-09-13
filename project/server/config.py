8894d231e15304a2d89d9e97f3554be257751cd6
Erin McAteer <60163579+emcateer27@users.noreply.github.com>
2021-09-13 17:39:25 -0400

    Converting back and fixing incorrect URL

--- project/server/config.py
+++ project/server/config.py
@@ -1,6 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-postgres_local_base = '//nhufnealkgurgp:56e0eae850a4a7656cafc7cd9684159a0daaf7bf5a558c8dac58c849dfee8b64@ec2-44-198-80-194.compute-1.amazonaws.com'
+postgres_local_base = 'postgres://nhufnealkgurgp:56e0eae850a4a7656cafc7cd9684159a0daaf7bf5a558c8dac58c849dfee8b64@ec2-44-198-80-194.compute-1.amazonaws.com'
 database_name = '5432/d67903pi0hptvl'
 
 
