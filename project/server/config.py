30490532b74aedcd0ef424e144ca9bd1032eee14
Erin McAteer <60163579+emcateer27@users.noreply.github.com>
2021-09-13 17:43:38 -0400

    Fixing incorrect URL part 2

--- project/server/config.py
+++ project/server/config.py
@@ -1,7 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-postgres_local_base = 'postgres://nhufnealkgurgp:56e0eae850a4a7656cafc7cd9684159a0daaf7bf5a558c8dac58c849dfee8b64@ec2-44-198-80-194.compute-1.amazonaws.com'
-database_name = '5432/d67903pi0hptvl'
+postgres_local_base = 'postgres://nhufnealkgurgp:56e0eae850a4a7656cafc7cd9684159a0daaf7bf5a558c8dac58c849dfee8b64@ec2-44-198-80-194.compute-1.amazonaws.com:5432'
+database_name = '/d67903pi0hptvl'
 
 
 class BaseConfig:
